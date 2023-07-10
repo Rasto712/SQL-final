3db5374c78424b1cd9d9a7f89556ddc72b59b39c
Rasto Mintal <rasto.mintal@gmail.com>
2023-07-10 19:40:39 +0200

    finished all the scripts

---
+++ Project-Employee_script.sql
@@ -0,0 +1,131 @@
+USE [CompanyDB]
+GO
+
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (1,123456)
+GO
+
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (1,123457)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (1,123458)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (1,123459)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (2,123460)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (2,123461)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (2,123462)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (3,123463)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (3,123464)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (3,123465)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (3,123466)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (4,123467)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (4,123468)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (4,123469)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (5,123470)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (5,123471)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (5,123472)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (5,123473)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (6,123474)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (6,123475)
+GO
+INSERT INTO [dbo].[Project-Employee]
+           ([ProjectID]
+           ,[EmployeeID])
+     VALUES
+           (6,123476)
+GO
+
