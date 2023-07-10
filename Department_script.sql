ac1076c8e1486326bdd37bb6889d253a82fa1dbb
Rasto Mintal <rasto.mintal@gmail.com>
2023-07-10 17:46:29 +0200

    added new scripts to fill up new tables in database

---
+++ Department_script.sql
@@ -0,0 +1,23 @@
+USE [CompanyDB]
+GO
+
+INSERT INTO [dbo].[Departments]
+           ([DepartmentID]
+           ,[name])
+     VALUES
+           (1,'IT')
+GO
+
+
+INSERT INTO [dbo].[Departments]
+           ([DepartmentID]
+           ,[name])
+     VALUES
+           (2,'BIO')
+GO
+INSERT INTO [dbo].[Departments]
+           ([DepartmentID]
+           ,[name])
+     VALUES
+           (3,'CHE')
+GO
---
+++ Project_script.sql
@@ -0,0 +1,47 @@
+USE [CompanyDB]
+GO
+
+INSERT INTO [dbo].[Projects]
+           ([Project ID]
+           ,[name]
+           ,[budget])
+     VALUES
+           (1,'Game development', 10000)
+GO
+
+INSERT INTO [dbo].[Projects]
+           ([Project ID]
+           ,[name]
+           ,[budget])
+     VALUES
+           (2,'Security software', 15200)
+GO
+INSERT INTO [dbo].[Projects]
+           ([Project ID]
+           ,[name]
+           ,[budget])
+     VALUES
+           (3,'Chemical explosions', 100000)
+GO
+INSERT INTO [dbo].[Projects]
+           ([Project ID]
+           ,[name]
+           ,[budget])
+     VALUES
+           (4,'Chemical reactions', 45000)
+GO
+INSERT INTO [dbo].[Projects]
+           ([Project ID]
+           ,[name]
+           ,[budget])
+     VALUES
+           (5,'Brain-study', 147000)
+GO
+INSERT INTO [dbo].[Projects]
+           ([Project ID]
+           ,[name]
+           ,[budget])
+     VALUES
+           (6,'Kidney-study', 1000000)
+GO
+
