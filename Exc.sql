0993169ae713f9950e03b569f425f96c70460621
Rasto Mintal <rasto.mintal@gmail.com>
2023-07-12 13:50:36 +0200

    third excersise done

--- Exc.sql
+++ Exc.sql
@@ -9,9 +9,20 @@ EMPLOYEESofDEPARTMENT as (select EmployeeID as EMP_id from [dbo].[Employees])
 select surname,name,EmployeeID from [dbo].[Employees] right join EMPLOYEES_IT on EMPLOYEES_IT.DEP_EMP_id=Employees.EmployeeID;
 
 -- druha uloha 
-
 with 
 PRJ_EMP as (select Distinct ProjectID from [dbo].[Project-Employee] ),
 project as (select ProjectID as PRid, COUNT(EmployeeID) as NO_of_EMP from [dbo].[Project-Employee] where ProjectID<7 group by ProjectID)
-select * from [dbo].[Projects] right join project on project.PRid=Projects.[Project ID]
+select * from [dbo].[Projects] right join project on project.PRid=Projects.[Project ID];
 
+-- tretia uloha
+
+with 
+deps as (select * from Departments),
+DEP_EMP as (select DepartmentID as DEPid, EmployeeID as EMPid from [dbo].[Department-Employee]),
+deps_DEP_EMP_join as (select * from DEP_EMP right join deps on deps.DepartmentID=DEP_EMP.DEPid),
+Deps_EMP_ID as (select EMPid,DEPid,name as Depname from deps_DEP_EMP_join),
+Salary as (select * from Deps_EMP_ID right join  [dbo].[Employees] on Deps_EMP_ID.EMPid= [dbo].[Employees].EmployeeID),
+Salary_final as (select DEPid, AVG(salary) as avg_salary from Salary where Depname='IT' or Depname='BIO' or Depname='CHE' group by DEPid  ),
+Final_join as (select name,DEPid,avg_salary from Salary_final left join deps on deps.DepartmentID=Salary_final.DEPid)
+select * from Final_join
+--
