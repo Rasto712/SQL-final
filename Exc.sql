cfe6a3b00abe63b53488c64381b90f5270e9fe28
Rasto Mintal <rasto.mintal@gmail.com>
2023-07-12 10:53:10 +0200

    excersise 2 competed

--- Exc.sql
+++ Exc.sql
@@ -1,10 +1,17 @@
-select DepartmentID from [dbo].[Departments]
-where name='IT';
+-- prva uloha
+with  DEP_IT as (
+select DepartmentID,name from [dbo].[Departments]
+where name='IT'),
 
-with EMPLOYEES_IT AS (select EmployeeID as DEP_EMP_id from [dbo].[Department-Employee] where DepartmentID=1),
-
+EMPLOYEES_IT AS (select EmployeeID as DEP_EMP_id from [dbo].[Department-Employee] where DepartmentID=1),
 EMPLOYEESofDEPARTMENT as (select EmployeeID as EMP_id from [dbo].[Employees])
 
 select surname,name,EmployeeID from [dbo].[Employees] right join EMPLOYEES_IT on EMPLOYEES_IT.DEP_EMP_id=Employees.EmployeeID;
---select * from EMPLOYEESofDEPARTMENT
+
+-- druha uloha 
+
+with 
+PRJ_EMP as (select Distinct ProjectID from [dbo].[Project-Employee] ),
+project as (select ProjectID as PRid, COUNT(EmployeeID) as NO_of_EMP from [dbo].[Project-Employee] where ProjectID<7 group by ProjectID)
+select * from [dbo].[Projects] right join project on project.PRid=Projects.[Project ID]
 
