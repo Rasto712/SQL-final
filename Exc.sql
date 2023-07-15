70b79f35b57afc4740c30a325590f7bdb35dd3e7
Rasto712 <36518353+Rasto712@users.noreply.github.com>
2023-07-15 15:07:55 +0200

    Update Exc.sql
    cleaned up the code
    removed unneeded lines

--- Exc.sql
+++ Exc.sql
@@ -19,15 +19,12 @@ select * from [dbo].[Projects] right join project on project.PRid=Projects.[Proj
 with 
 deps as (select * from Departments),
 DEP_EMP as (select DepartmentID as DEPid, EmployeeID as EMPid from [dbo].[Department-Employee]),
-deps_DEP_EMP_join as (select * from DEP_EMP right join deps on deps.DepartmentID=DEP_EMP.DEPid),
-Deps_EMP_ID as (select EMPid,DEPid,name as Depname from deps_DEP_EMP_join),
+Deps_EMP_ID as (select EMPid,DEPid,name as Depname from DEP_EMP right join deps on deps.DepartmentID=DEP_EMP.DEPid),
 Salary as (select * from Deps_EMP_ID right join  [dbo].[Employees] on Deps_EMP_ID.EMPid= [dbo].[Employees].EmployeeID),
-Salary_final as (select DEPid, AVG(salary) as avg_salary from Salary where Depname='IT' or Depname='BIO' or Depname='CHE' group by DEPid  ),
+Salary_final as (select DEPid, AVG(salary) as avg_salary from Salary group by DEPid  ),
 Final_join as (select name,DEPid,avg_salary from Salary_final left join deps on deps.DepartmentID=Salary_final.DEPid)
 select * from Final_join;
 
 --stvrta uloha 
 update Employees set salary=5000 where EmployeeID=123456
 select * from Employees where EmployeeID=123456;
-
-
